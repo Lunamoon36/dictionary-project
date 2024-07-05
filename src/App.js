0cb088a8389c2e29576e72778942e0f6bc1d4ba6
Lunamoon36 <150391744+Lunamoon36@users.noreply.github.com>
2024-07-05 20:02:41 +0100

    Built the basic form and applied some basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" className="App-logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>{" "}
+        <footer className="App-footer">
+          <small>Coded by Cherry Atkinson</small>
+        </footer>
+      </div>
     </div>
   );
 }
