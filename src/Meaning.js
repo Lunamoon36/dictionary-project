e4a05b1f73a0beb1d056d911a0e2c1e7bb779b3a
Lunamoon36 <150391744+Lunamoon36@users.noreply.github.com>
2024-07-15 11:09:26 +0100

    Rendered the word meanings

---
+++ src/Meaning.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <p>
+        <strong>Definition:</strong>
+        {props.meaning.definition}
+        <br />
+        <strong>Example:</strong>
+        <em>{props.meaning.example}</em>
+        <br />
+      </p>
+    </div>
+  );
+}
