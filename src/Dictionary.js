8ef6722f70f8475e2b697276d32333ffc05cc0ca
Lunamoon36 <150391744+Lunamoon36@users.noreply.github.com>
2024-07-18 10:56:57 +0100

    created phonetic component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,17 +8,17 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    setResults(response.data);
+    setResults(response.data[0]);
   }
+
   function search(event) {
     event.preventDefault();
 
-    let apiKey = "773b1fb672t5o4902c27a62f4db0b82a";
-    //documentation: https://www.shecodes.io/learn/apis/dictionary
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
